29927cc969098a741471862a93bc2aab22440a22
Lea Griffon <lea.griffondor@gmail.com>
2025-11-27 13:16:45 +0100

    gigik

--- packages/web/src/util/baseUrl.ts
+++ packages/web/src/util/baseUrl.ts
@@ -1,3 +1,3 @@
 // src/util/baseUrl.ts
-const baseUrl = 'http://sanger.dia.fi.upm.es/pui-rest-news/login'
+const baseUrl = 'http://sanger.dia.fi.upm.es/pui-rest-news/articles'
 export default baseUrl
